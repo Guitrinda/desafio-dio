d6aa65e8152ac67100f46a6fe742166cdf773634
Guitrinda <89198037+Guitrinda@users.noreply.github.com>
2021-12-23 09:14:02 -0300

    Delete

--- Estudos-Cursos/My-SQL/check_prompt.sql
+++
@@ -1,9 +0,0 @@
--- prompt comando
-
-mysqlcheck -u root -p curso senso -–repair -–quick
-
-mysqlcheck -u root -p  curso senso --repair --force -v
- 
-mysqlcheck -u root -p  curso  -v
-
-mysqlcheck -u root -p  curso --repair --force -v
